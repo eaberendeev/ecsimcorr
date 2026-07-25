a7138c1bd2b550bb01ce11677812853486b69362
Deneb <Kirill.ufimtzev@yandex.ru>
2026-07-25 13:18:27 +0700

    21: written random Lehmer64 engine

---
+++ srcBeren/utils/random.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstdint>
+#include <random>
+
+/* While C++ random generators has linear complexity of discard function, this has logarithmic complexity
+ * Inspired by
+ * https://lemire.me/blog/2019/03/19/the-fastest-conventional-random-number-generator-that-can-pass-big-crush/
+ * https://github.com/lemire/testingRNG/blob/master/source/lehmer64.h
+ */
+struct LehmerEngine {
+    LehmerEngine(uint64_t seed) {
+        state = seed + 1;
+        discard(seed + 2);
+    }
+
+    uint64_t operator()() {
+        state *= magicConstant;
+        return state >> 64;
+    }
+
+    void discard(int64_t count) {
+        __uint128_t mult = magicConstant;
+        for (uint64_t mask = 1; mask <= count; mask *= 2) {
+            if (mask & count) {
+                state *= mult;
+            }
+            mult *= mult;
+        }
+    }
+
+    static uint64_t min() {
+        return 0;
+    }
+
+    static uint64_t max() {
+        return ~(0UL);
+    }
+
+    __uint128_t state;
+
+    static constexpr uint64_t magicConstant = 0xda942042e4dd58b5ull;
+};
