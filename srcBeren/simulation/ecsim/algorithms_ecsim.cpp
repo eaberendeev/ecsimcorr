8fda9d0a6742bb835b56983fe4ad0565bade8580
Deneb <Kirill.ufimtzev@yandex.ru>
2026-09-06 00:11:26 +0700

    48: init fieldEp with zero at first step and 2nd problem field in this place too

--- srcBeren/simulation/ecsim/algorithms_ecsim.cpp
+++ srcBeren/simulation/ecsim/algorithms_ecsim.cpp
@@ -96,8 +96,6 @@ void predict_current_impl_linear(const ParticlesArray& particles, const Field3d&
 void calculate_current(const ParticlesArray& particles, Field3d& fieldJ) {
     RECORD_TIMER;
 
-    fieldJ.setZero();
-
     const double qp = particles.charge;
     const double mpw = particles.mpw();
     const auto& domain = particles.get_domain();
